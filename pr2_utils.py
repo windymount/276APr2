d6c7e0927629c86e97d196b5847de05242c3c39b
gyan <1700010674@pku.edu.cn>
2022-02-28 21:06:46 -0800

    Final commit

--- pr2_utils.py
+++ pr2_utils.py
@@ -130,28 +130,6 @@ def mapCorrelation(im, x_im, y_im, vp, xs, ys, ptype):
     return cpr
 
 
-def my_map_correlation(im, x_im, y_im, vp, ptype):
-    """
-    
-    Slightly changed MapCorrelation.
-    Add ptype which specify we expect an obstacle(1) or free(0)
-    
-    """
-    nx = im.shape[0]
-    ny = im.shape[1]
-    xmin = x_im[0]
-    xmax = x_im[-1]
-    xresolution = (xmax-xmin)/(nx-1)
-    ymin = y_im[0]
-    ymax = y_im[-1]
-    yresolution = (ymax-ymin)/(ny-1)
-    y1 = vp[1,:] # 1 x 1076
-    iy = np.int16(np.round((y1-ymin)/yresolution))
-    x1 = vp[0,:] # 1 x 1076
-    ix = np.int16(np.round((x1-xmin)/xresolution))
-    points = im[ix,iy]
-    cpr = np.sum(points * ptype) - np.sum(np.log(1 + np.exp(points)))
-    return cpr
 
 
 def bresenham2D(sx, sy, ex, ey):
@@ -217,96 +195,6 @@ def test_bresenham2D():
         x,y = bresenham2D(sx, sy, 500, 200)
     print("1000 raytraces: --- %s seconds ---" % (time.time() - start_time))
 
-
-def test_mapCorrelation():
-    _, lidar_data = read_data_from_csv('data/sensor_data/lidar.csv')
-    angles = np.linspace(-5, 185, 286) / 180 * np.pi
-    ranges = lidar_data[0, :]
-
-    # take valid indices
-    indValid = np.logical_and((ranges < 80),(ranges> 0.1))
-    ranges = ranges[indValid]
-    angles = angles[indValid]
-
-    # init MAP
-    MAP = {}
-    MAP['res']   = 0.1 #meters
-    MAP['xmin']  = -50  #meters
-    MAP['ymin']  = -50
-    MAP['xmax']  =  50
-    MAP['ymax']  =  50 
-    MAP['sizex']  = int(np.ceil((MAP['xmax'] - MAP['xmin']) / MAP['res'] + 1)) #cells
-    MAP['sizey']  = int(np.ceil((MAP['ymax'] - MAP['ymin']) / MAP['res'] + 1))
-    MAP['map'] = np.zeros((MAP['sizex'],MAP['sizey']),dtype=np.int8) #DATA TYPE: char or int8
-    
-    #import pdb
-    #pdb.set_trace()
-    
-    # xy position in the sensor frame
-    xs0 = ranges*np.cos(angles)
-    ys0 = ranges*np.sin(angles)
-    
-    # convert position in the map frame here 
-    Y = np.stack((xs0,ys0))
-    
-    # convert from meters to cells
-    xis = np.ceil((xs0 - MAP['xmin']) / MAP['res'] ).astype(np.int16)-1
-    yis = np.ceil((ys0 - MAP['ymin']) / MAP['res'] ).astype(np.int16)-1
-    
-    # build an arbitrary map 
-    indGood = np.logical_and(np.logical_and(np.logical_and((xis > 1), (yis > 1)), (xis < MAP['sizex'])), (yis < MAP['sizey']))
-    MAP['map'][xis[indGood],yis[indGood]]=1
-        
-    x_im = np.arange(MAP['xmin'],MAP['xmax']+MAP['res'],MAP['res']) #x-positions of each pixel of the map
-    y_im = np.arange(MAP['ymin'],MAP['ymax']+MAP['res'],MAP['res']) #y-positions of each pixel of the map
-
-    x_range = np.arange(-0.4,0.4+0.1,0.1)
-    y_range = np.arange(-0.4,0.4+0.1,0.1)
-
-
-    
-    print("Testing map_correlation with {}x{} cells".format(MAP['sizex'],MAP['sizey']))
-    ts = tic()
-    c = mapCorrelation(MAP['map'],x_im,y_im,Y,x_range,y_range)
-    print(c)
-    toc(ts,"Map Correlation")
-
-    c_ex = np.array([[ 4.,  6.,  6.,  5.,  8.,  6.,  3.,  2.,  0.],
-                    [ 7.,  5., 11.,  8.,  5.,  8.,  5.,  4.,  2.],
-                    [ 5.,  7., 11.,  8., 12.,  5.,  2.,  1.,  5.],
-                    [ 6.,  8., 13., 66., 33.,  4.,  3.,  3.,  0.],
-                    [ 5.,  9.,  9., 63., 55., 13.,  5.,  7.,  4.],
-                    [ 1.,  1., 11., 15., 12., 13.,  6., 10.,  7.],
-                    [ 2.,  5.,  7., 11.,  7.,  8.,  8.,  6.,  4.],
-                    [ 3.,  6.,  9.,  8.,  7.,  7.,  4.,  4.,  3.],
-                    [ 2.,  3.,  2.,  6.,  8.,  4.,  5.,  5.,  0.]])
-        
-    if np.sum(c==c_ex) == np.size(c_ex):
-        print("...Test passed.")
-    else:
-        print("...Test failed. Close figures to continue tests.")	
-
-    #plot original lidar points
-    fig1 = plt.figure()
-    plt.plot(xs0,ys0,'.k')
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.title("Laser reading")
-    plt.axis('equal')
-
-    #plot map
-    fig2 = plt.figure()
-    plt.imshow(MAP['map'],cmap="hot");
-    plt.title("Occupancy grid map")
-    
-    #plot correlation
-    fig3 = plt.figure()
-    ax3 = fig3.gca(projection='3d')
-    X, Y = np.meshgrid(np.arange(0,9), np.arange(0,9))
-    ax3.plot_surface(X,Y,c,linewidth=0,cmap=plt.cm.jet, antialiased=False,rstride=1, cstride=1)
-    plt.title("Correlation coefficient map")
-
-    plt.show()
     
     
 def show_lidar():
